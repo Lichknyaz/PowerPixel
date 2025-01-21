1ac1026b475741dd2df43a4da815d939f7ccc386
yanashapka <yana.shapka.test@gmail.com>
2025-01-21 18:07:58 +0200

    mobile menu

---
+++ src/js/mobilemenu.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const menuButton = document.querySelector('.js-open-menu');
+  const menuCloseButton = document.querySelector('.js-close-menu');
+  const mobileMenu = document.querySelector('.menu-backdrop');
+
+  if (menuButton && mobileMenu) {
+    menuButton.addEventListener('click', function () {
+      console.log('!!!');
+      mobileMenu.classList.add('is-open');
+    });
+
+    menuCloseButton.addEventListener('click', function () {
+      mobileMenu.classList.remove('is-open');
+    });
+  }
+});
