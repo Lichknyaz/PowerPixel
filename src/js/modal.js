45ee38c9962340e242a3b58e205a5d2b8b4e1364
yanashapka <yana.shapka.test@gmail.com>
2025-01-24 19:11:27 +0200

    fix

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
   const starIcon = document.querySelector('.sun-icon');
+  const modal = document.querySelector('.modal');
   const modalCloseButton = document.querySelector('.modal-close');
+
+  if (starIcon && modal && modalCloseButton) {
+    starIcon.addEventListener('click', function () {
+      modal.classList.add('is-open');
+      modalCloseButton.style.display = 'block';
+    });
+
+    modalCloseButton.addEventListener('click', function () {
+      modal.classList.remove('is-open');
+      modalCloseButton.style.display = 'none';
+    });
+  }
 });
