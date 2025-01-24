9b9ed7d948590d7c35ed71d098d2899d935f52d1
yanashapka <yana.shapka.test@gmail.com>
2025-01-24 14:48:47 +0200

    modal

---
+++ src/js/modal.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const starIcon = document.querySelector('.sun-icon');
+  const modalCloseButton = document.querySelector('.modal-close');
+});
